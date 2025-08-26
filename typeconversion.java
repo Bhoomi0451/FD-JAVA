a5d48ceb07f0e0b316c26ad2257b35c56cfb7d72
Bhoomi Tiwari <bhoomi2856@gmail.com>
2025-08-26 09:54:28 +0530

    Add files via upload

---
+++ typeconversion.java
@@ -0,0 +1,18 @@
+public class typeconversion {
+    public static void main(String[] args) {
+        // int num=100;
+        // long bigNum = num;
+        // float floatNum=bigNum;
+
+        // System.out.println("Int"+num);
+        // System.out.println("Long"+bigNum);
+        // System.out.println("Float"+floatNum);
+
+
+        double d=100.99;
+        int n=(int) d;
+        System.out.println(d);
+        System.out.println(n);
+    }
+    
+}
